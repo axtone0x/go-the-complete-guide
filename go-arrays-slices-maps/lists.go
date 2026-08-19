fefbb49235a57c54f9177a99c89ff7bef9849ea6
axtone0x <oceballos001@gmail.com>
2026-08-19 08:13:43 -0600

    Slices, length and capacity analysis.

--- go-arrays-slices-maps/lists.go
+++ go-arrays-slices-maps/lists.go
@@ -14,12 +14,25 @@ type TemperatureData struct {
 }
 
 func main() {
-	var productNames [4]string = [4]string{"Spoon"}
-	prices := [4]float64{10.9,9.99,15.25,0.77}
+	//var productNames [4]string = [4]string{"Spoon"}
+	prices := [7]float64{100.1,200.2,300.3,400.4,500.5,600.6,700.7}
+	fmt.Printf("Len -> %v, Cap -> %v", len(prices), cap(prices))
 
-	fmt.Println(prices)
-	fmt.Println(productNames)
+	// fmt.Println(prices)
+	// fmt.Println(productNames)
 
-	fmt.Println(prices[3])
-	fmt.Println(productNames[3])
+	// fmt.Println(prices[3])
+	// fmt.Println(productNames[0])
+
+	//featuredPrices := prices[0:3]
+	//featuredPrices := prices[:3] //0 - 2 index
+	featuredPrices := prices[5:] // 200 - 600
+	//featuredPrices[2] = 4000.4
+	fmt.Println(featuredPrices)
+	fmt.Println(len(featuredPrices),cap(featuredPrices))
+
+	highlightedPrices := featuredPrices[:1] //200 - 600
+	fmt.Println(highlightedPrices)
+
+	fmt.Println(len(highlightedPrices),cap(highlightedPrices))
 }
